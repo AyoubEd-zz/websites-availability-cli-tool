2c576f288b8244d2fa1e850581f1854c4d95b106
aeddakhly <ayoube.eddakhly@gmail.com>
2020-06-28 11:22:01 +0200

    stats process with a smilpe ticker

--- statsagent/statsagent.go
+++ statsagent/statsagent.go
@@ -2,6 +2,7 @@ package statsagent
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 	"time"
 
@@ -18,19 +19,20 @@ type WebsiteStat struct {
 }
 
 type websiteStatsQueue struct {
+	ID              int
 	refreshInterval int64
+	displayInterval int64
 	queue           []request.ResponseLog
 	mux             sync.Mutex
 }
 
+// ProcessLogs receives logs from the ping agent andf stores the latest logs in memory
 func ProcessLogs(logc chan request.ResponseLog, errc chan error) {
 	done := make(chan bool, 1)
 
-	ws1, ws2 := websiteStatsQueue{refreshInterval: 10, queue: []request.ResponseLog{}}, websiteStatsQueue{refreshInterval: 60, queue: []request.ResponseLog{}}
+	ws1, ws2 := websiteStatsQueue{ID: 1, refreshInterval: 60, displayInterval: 10, queue: []request.ResponseLog{}}, websiteStatsQueue{ID: 2, refreshInterval: 3600, displayInterval: 60, queue: []request.ResponseLog{}}
 	go ws1.removeOutdatedLogs(done, errc)
 	go ws2.removeOutdatedLogs(done, errc)
-	go ws1.startTicker(done, errc)
-	go ws2.startTicker(done, errc)
 	for log := range logc {
 		ws1.Add(log)
 		ws2.Add(log)
@@ -45,18 +47,23 @@ func (c *websiteStatsQueue) Add(log request.ResponseLog) {
 
 func (c *websiteStatsQueue) removeOutdatedLogs(done chan bool, errc chan error) {
 	timer := time.NewTicker(time.Duration(10) * time.Second)
+	var ticks int64 = 0
 
 	for {
 		select {
 		case t := <-timer.C:
+			ticks++
 			c.mux.Lock()
 			newQueue := make([]request.ResponseLog, 0)
 			for _, lg := range c.queue {
-				if t.Unix()-lg.Timestamp <= 10 {
+				if t.Unix()-lg.Timestamp <= c.refreshInterval {
 					newQueue = append(newQueue, lg)
 				}
 			}
 			c.queue = newQueue
+			if (ticks*10)%c.displayInterval == 0 {
+				go sendUpdatedStats(c.ID, c.queue, t, c.refreshInterval/6)
+			}
 			c.mux.Unlock()
 		case <-done:
 			timer.Stop()
@@ -65,20 +72,13 @@ func (c *websiteStatsQueue) removeOutdatedLogs(done chan bool, errc chan error) 
 	}
 }
 
-func (c *websiteStatsQueue) startTicker(done chan bool, errc chan error) {
-	timer := time.NewTicker(time.Duration(c.refreshInterval) * time.Second)
-
-	for {
-		select {
-		case t := <-timer.C:
-			c.mux.Lock()
-			fmt.Println(t, "--------------------------------------------", c.refreshInterval)
-			for _, lg := range c.queue {
-				fmt.Println(time.Unix(lg.Timestamp, 0), lg.URL)
-			}
-			c.mux.Unlock()
-		case <-done:
-			timer.Stop()
-		}
+func sendUpdatedStats(ID int, q []request.ResponseLog, t time.Time, inter int64) {
+	sortedLogs := q
+	sort.Slice(sortedLogs, func(a, b int) bool {
+		return sortedLogs[a].Timestamp < sortedLogs[b].Timestamp
+	})
+	fmt.Println("[", ID, "]", t, "--------------------------------------------", inter)
+	for _, lg := range sortedLogs {
+		fmt.Println(time.Unix(lg.Timestamp, 0), lg.URL)
 	}
 }
