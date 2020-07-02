f0bd11220e0625999ebde7621bb6cdbfe13a78f1
aeddakhly <ayoube.eddakhly@gmail.com>
2020-07-02 05:07:08 +0200

    add availability to dashboard stats

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -1,28 +1,3 @@
-// package dashboard
-
-// import (
-// 	"fmt"
-// 	"time"
-
-// 	"github.com/ayoubed/datadog-home-project/statsagent"
-// )
-
-// func Run(urls []string, interval, span int) {
-// 	go func() {
-// 		ticker := time.NewTicker(time.Duration(interval) * time.Second)
-// 		for {
-// 			select {
-// 			case t := <-ticker.C:
-// 				fmt.Printf("[last %vs stats]---------------------%v-------------------\n", interval, t)
-// 				res := statsagent.GetStats(urls, span)
-// 				for k, v := range res {
-// 					fmt.Println("> ", k)
-// 					fmt.Printf("%+v\n", v)
-// 				}
-// 			}
-// 		}
-// 	}()
-// }
 package dashboard
 
 import (
@@ -33,6 +8,7 @@ import (
 	"time"
 
 	"github.com/ayoubed/datadog-home-project/statsagent"
+	"github.com/fatih/color"
 	"github.com/jroimartin/gocui"
 )
 
@@ -51,6 +27,35 @@ func Run(urls []string, views []View) {
 
 	g.SetManagerFunc(layout(g, views))
 
+	updateViews(views, g, urls)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("stdin", gocui.KeyArrowUp, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error {
+			scrollView(v, -1)
+			return nil
+		}); err != nil {
+
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("stdin", gocui.KeyArrowDown, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error {
+			scrollView(v, 1)
+			return nil
+		}); err != nil {
+
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func updateViews(views []View, g *gocui.Gui, urls []string) {
+
 	for index := range views {
 		go func(currentIndex int, currentView *View) {
 			ticker := time.NewTicker(time.Duration(currentView.UpdateInterval) * time.Second)
@@ -63,34 +68,25 @@ func Run(urls []string, views []View) {
 						if err != nil {
 							return err
 						}
-						v.Title = fmt.Sprintf("Statistics for the last %vs(updated every %vs)", currentView.UpdateInterval, currentView.TimeFrame)
-						v.FgColor = gocui.ColorCyan
-						v.SelBgColor = gocui.ColorBlue
-						v.SelFgColor = gocui.ColorBlack
-						v.Wrap = true
 						v.Clear()
-						fmt.Fprintln(v, fmt.Sprintf("%-30v %25v %25v %25v %25v %+v\n", "Website", "Average Response Time", "Max Response Time", "Avg TTFB", "Max TTFB", "Status Codes"))
-						for k, value := range res {
+
+						blue := color.New(color.FgYellow)
+						blue.Fprintln(v, fmt.Sprintf("%-30v %20v %21v %21v %21v %21v %25v\n", "Website", "Availability", "Average Response Time", "Max Response Time", "Avg TTFB", "Max TTFB", "Status Codes"))
+
+						for _, url := range urls {
+							value := res[url]
 							statusCodeSlice := make([]string, 0)
 							for code, count := range value.StatusCodeCount {
 								statusCodeSlice = append(statusCodeSlice, fmt.Sprintf("%v:%v", code, count))
 							}
-							statusCodeStr := fmt.Sprintf("(%v)", strings.Join(statusCodeSlice, " "))
-							fmt.Fprintln(v, fmt.Sprintf("%-30v %25v %25v %25v %25v %-25v\n", k, value.AvgResponseTime, value.MaxResponseTime, value.AvgTimeToFirstByte, value.MaxTimeToFirstByte, statusCodeStr))
+							statusCodeStr := fmt.Sprintf("[%v]", strings.Join(statusCodeSlice, " "))
+							fmt.Fprintln(v, fmt.Sprintf("%-30v %20v %21v %21v %21v %21v %25v", url, value.Availability, value.AvgResponseTime, value.MaxResponseTime, value.AvgTimeToFirstByte, value.MaxTimeToFirstByte, statusCodeStr))
 						}
 						return nil
 					})
 				}
 			}
 		}(index, &views[index])
-	}
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
 	}
 }
 
@@ -99,15 +95,29 @@ func layout(g *gocui.Gui, views []View) func(*gocui.Gui) error {
 	return func(g *gocui.Gui) error {
 		for index, view := range views {
 			v, err := g.SetView(strconv.Itoa(view.TimeFrame), 0, index*(maxY/3), maxX, (index+1)*(maxY/3))
+			v.FgColor = gocui.ColorCyan
 			if err != nil {
 				if err != gocui.ErrUnknownView {
 					log.Panic("Error setting views")
 				}
-				fmt.Fprintln(v, "One moment, we're getting the lastest statistics...")
+				fmt.Fprintln(v, fmt.Sprintf("One moment, we're getting statistics for the last %vs...", view.TimeFrame))
 			}
+			v.Title = fmt.Sprintf(" Statistics for the last %vs (updated every %vs) ", view.TimeFrame, view.UpdateInterval)
+			v.Wrap = true
 		}
 		return nil
 	}
+}
+
+func scrollView(v *gocui.View, dy int) error {
+	if v != nil {
+		v.Autoscroll = false
+		ox, oy := v.Origin()
+		if err := v.SetOrigin(ox, oy+dy); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ayoubed/datadog-home-project
 go 1.14
 
 require (
+	github.com/fatih/color v1.9.0
 	github.com/influxdata/influxdb v1.8.0
 	github.com/jroimartin/gocui v0.4.0
 	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1 // indirect
